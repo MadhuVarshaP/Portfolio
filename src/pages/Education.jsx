7f2c9e5a56cc8b292d289e29923095d6c86b6c4f
MADHUthecoder <2k22csbs26@kiot.ac.in>
2024-02-19 23:10:27 +0530

    color updated

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -9,27 +9,27 @@ function Education() {
     <li class="mb-10 ms-4 w-[300px]">
         <div class="absolute w-3 h-3 bg-black rounded-full ml-[-14px] mt-1.5 -start-5.5 border border-black dark:border-gray-900 dark:bg-gray-700"></div>
         <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"><span className='font-secondary'>2022</span>-Ongoing</time>
-        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">B.Tech Computer Science and Business Systems</h3>
-        <p class="text-base font-normal text-gray-500 dark:text-gray-400">CGPA: <span className='font-secondary'>8.85</span></p>
+        <h3 class="text-lg font-semibold text-gray-900 dark:text-black">B.Tech Computer Science and Business Systems</h3>
+        <p class="text-base font-normal text-gray-500 dark:text-black">CGPA: <span className='font-secondary'>8.85</span></p>
         <path class="" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
     </li>
     <li class="mb-10 ms-4 w-[300px]">
         <div class="absolute w-3 h-3 bg-black rounded-full ml-[-14px] mt-1.5 -start-5.5 border border-black dark:border-gray-900 dark:bg-gray-700"></div>
         <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"><span className='font-secondary'>2022</span>-Ongoing</time>
-        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Diploma in Data Science and Programming</h3>
-        <p class="text-base font-normal text-gray-500 dark:text-gray-400">CGPA: <span className='font-secondary'>7.00</span></p>
+        <h3 class="text-lg font-semibold text-gray-900 dark:text-black">Diploma in Data Science and Programming</h3>
+        <p class="text-base font-normal text-gray-500 dark:text-black">CGPA: <span className='font-secondary'>7.00</span></p>
     </li>
     <li class="mb-10 ms-4 w-[300px]">
         <div class="absolute w-3 h-3 bg-black rounded-full ml-[-14px] mt-1.5 -start-5.5 border border-black dark:border-gray-900 dark:bg-gray-700"></div>
         <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"><span className='font-secondary'>2020-2022</span></time>
-        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Grade <span className='font-secondary'>12</span></h3>
-        <p class="text-base font-normal text-gray-500 dark:text-gray-400">Percentage: <span className='font-secondary'>87.6%</span></p>
+        <h3 class="text-lg font-semibold text-gray-900 dark:black">Grade <span className='font-secondary'>12</span></h3>
+        <p class="text-base font-normal text-gray-500 dark:text-black">Percentage: <span className='font-secondary'>87.6%</span></p>
     </li>
     <li class="mb-10 ms-4 w-[300px]">
         <div class="absolute w-3 h-[12px] bg-black rounded-full ml-[-14px] mt-1.5 -start-5.5 border border-black dark:border-gray-900 dark:bg-gray-700"></div>
         <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"><span className='font-secondary'>2018-2020</span></time>
-        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Grade <span className='font-secondary'>10</span></h3>
-        <p class="text-base font-normal text-gray-500 dark:text-gray-400">Percentage: <span className='font-secondary'>94.8%</span></p>
+        <h3 class="text-lg font-semibold text-gray-900 dark:text-black">Grade <span className='font-secondary'>10</span></h3>
+        <p class="text-base font-normal text-gray-500 dark:text-black">Percentage: <span className='font-secondary'>94.8%</span></p>
     </li>
 </ol>
 
